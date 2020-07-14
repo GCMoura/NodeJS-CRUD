78e336851fc4afcb566ec469e546b7ee5b9dbbce
GCMoura <51333439+GCMoura@users.noreply.github.com>
2020-07-13 21:00:57 -0300

    atualização

--- index.js
+++ index.js
@@ -15,13 +15,43 @@ const writeFile = (content) => {
     fs.writeFileSync('./data/db.json', updateFile, 'utf-8')
 }
 
+const verifyData = (flag, db, data) => {
+    for(let i = 0; i < db.length; i++){
+        if(db[i].name === data.name && db[i].email === data.email) {
+            console.log('Usuário já existe')
+            flag = true
+        } 
+    }
+    return flag
+}
+
 router.get('/', (req, res) => {
     const content = readFile()
     res.send(content)
 })
 
-router.post('/user', (req, res) => {
-    res.send('Teste post')
+router.post('/', (req, res) => {
+    const { name, email, phone } = req.body
+    const currentData = readFile()
+
+    const id = Math.random().toString(32).substr(2, 9)
+
+    var flag = false
+
+    const returnVerifyData = verifyData(flag, currentData, {name, email})
+
+    if(returnVerifyData == false) {
+        currentData.push({ id, name, email, phone})
+        writeFile(currentData)
+        res.send({ id, name, email, phone })
+    } else {
+        res.send('Usuário já cadastrado')
+    }
+})
+
+router.put('/:id', (req, res) => {
+    const {id} = req.params
+    console.log(id)
 })
 
 server.use(router)
